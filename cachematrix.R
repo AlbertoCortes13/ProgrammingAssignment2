ad4a10205b40c6b5dc39368309345de5a3f32aa6
Alberto Cortes <a.cortes.guerrero@accenture.com>
2016-07-27 11:41:25 -0500

    Creates two functions that can cache the inverse of a matrix
    The first function creates a list containing functions to: set a matrix,
    
    get a matrix, set the inverse of the matrix, get the inverse of the
    matrix.
    
    The second function shows the inverse of a matrix and if the inverse has
    already been calculated, the cache will show the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create two functions that can cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a list containing functions to:
+##set a matrix
+##get a matrix
+##set the inverse of the matrix
+##get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <- NULL ##m is used to store the inverse to cache
+    
+    set <- function(y){
+        x <<- y ##Assigns a matrix to y
+        m <<- NULL ##Inverse in cache is set to NULL
+    }
+    
+    get <- function() x ##Shows the matrix function
+    setInverse <- function(inverse) m <<- inverse ##Sets the inverse to m to store it to the cache
+    getInverse <- function() m ##shows the inverse of the matrix saved in cache
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) ##The variables are stored in a list
 }
 
 
-## Write a short comment describing this function
+## This function shows the inverse of a matrix and if the inverse has already been calculated, the cache will show the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getInverse() ##assigns the inverse of a matrix to m
+    
+    ##If there's an inverse matrix already, show a message and return the value of m
+    if (!is.null(m)) {
+        message("Getting cache data")
+        return(m)
+    }
+    
+    data <- x$get() ## get the matrix without the inverse
+    m <- ginv(data, ...) ##calculates the inverse of the matrix
+    x$setInverse(m) ##sets the inverse of m
+    m
 }
